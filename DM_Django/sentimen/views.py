8e50f6dedf3f3317926d97d25be3dfe764a30f7f
filipusarif <filipus123.gmil@gmail.com>
2023-12-14 18:41:26 +0700

    add visualization data

--- DM_Django/sentimen/views.py
+++ DM_Django/sentimen/views.py
@@ -5,6 +5,9 @@ import pandas as pd
 import numpy as np
 import re
 import sklearn
+# import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
 
 from .models import Analisis
 from start.models import DataTesting
@@ -89,8 +92,98 @@ def index(request):
     # print(y_test,X_test)
     # html_string = df.to_html(index=False)
     # mengubah data frame menjadi dictionary
-
+    # positive = Analisis.objects.filter(sentimen='positive').count() # menghitung jumlah data sentimen positif
+    # neutral = Analisis.objects.filter(sentimen='neutral').count() # menghitung jumlah data sentimen netral
+    # negative = Analisis.objects.filter(sentimen='negative').count() # menghitung jumlah data sentimen negatif
+    # sentimen = [positive, neutral, negative] # membuat list yang berisi jumlah data sentimen
+    # label = ['Positive', 'Neutral', 'Negative'] # membuat list yang berisi label sentimen
+    # plt.bar(label, sentimen) # membuat bar plot dari data sentimen dan label
+    # plt.xlabel('Sentimen') # memberi label sumbu x
+    # plt.ylabel('Jumlah Data') # memberi label sumbu y
+    # plt.title('Visualisasi Data Sentimen') # memberi judul plot
+    # plt.savefig('static/asset/visual/sentimen.png') # menyimpan plot sebagai file gambar dengan nama 'sentimen.png'
+    # Visualsisasi Sentimen
+    figSentimen = px.pie(df, names='sentimen', title = 'Sentimen', width=800, height=400)
+    figSentimen.update_layout(
+        margin=dict(l=20, r=20, t=20, b=20),
+        paper_bgcolor="#ff0000",
+    )
+    chart = figSentimen.to_html()
     data_dict = df.to_dict(orient='records')
+
+    # visualisasi Word count
+    countWord = Analisis.objects.values_list('text', flat=True)
+    words = []
+    for text in countWord:
+        cleaned_text = re.sub(r'[^\w\s]', '', text)
+        words.extend(cleaned_text.lower().split())
+    word_count = {}
+    for word in words:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+    top_words = sorted(word_count.items(), key=lambda x: x[1], reverse=True)[:10]
+    df_top_words = pd.DataFrame(top_words, columns=['kata', 'jumlah'])
+    # data_word = px.Analisis.gapminder().query("country == 'Canada'")
+    figWord = px.bar(df_top_words, x='kata', y='jumlah', title="10 Kata Teratas",)
+    figWord.update_layout(
+        xaxis_title="Kata",
+        yaxis_title="Jumlah",
+    )
+    chartWord = figWord.to_html()
+
+    y_preds_train = model_g.predict(X_train)
+
+    # figTrain = px.histogram(x=y_preds_train, title="Distribusi Hasil Prediksi Data Training", labels={'x': 'Sentimen'})
+    # figTrain.update_layout(
+    #     xaxis_title="Sentimen",
+    #     yaxis_title="Jumlah",
+    # )
+    # chartTrain = figTrain.to_html()
+    
+    # figTrainScatter = px.scatter(x=range(len(y_preds_train)), y=y_preds_train, title="Hasil Prediksi Data Training", labels={'x': 'Data', 'y': 'Sentimen'})
+    # figTrainScatter.update_layout(
+    #     xaxis_title="Data",
+    #     yaxis_title="Sentimen",
+    # )
+    # chartTrain = figTrainScatter.to_html()
+
+    y_preds = model_g.predict(X_test)
+    cm = confusion_matrix(y_test, y_preds)
+
+    figConfusionMatrix = go.Figure(data=go.Heatmap(
+        z=cm,
+        x=['Predicted Negative', 'Predicted Positive'],
+        y=['True Negative', 'True Positive'],
+        colorscale='Viridis',
+        reversescale=True,
+    ))
+
+    figConfusionMatrix.update_layout(
+        title='Confusion Matrix Heatmap',
+        xaxis_title='Predicted Label',
+        yaxis_title='True Label',
+    )
+
+    chartTrain = figConfusionMatrix.to_html()
+
+    
+    target_names = ['negative', 'positive']
+
+    # target_names = ['Negative', 'Positive']
+    classification_rep = classification_report(y_test, y_preds, target_names=target_names, output_dict=True)
+    figClassificationReport = px.bar(
+        x=target_names * 3,  # Repeat class names for each metric
+        y=[classification_rep[class_name.lower()][metric] for class_name in target_names for metric in ['precision', 'recall', 'f1-score']],
+        color=['Precision', 'Recall', 'F1-Score'] * len(target_names),
+        barmode='group',
+        labels={'y': 'Score', 'x': 'Class', 'color': 'Metric'},
+        title='Classification Report Metrics by Class',
+    )
+
+    chartClassificationReport = figClassificationReport.to_html()
+
     context = {
         'data_sentimen' : data_dict,
         'matrix' : '11',
@@ -102,6 +195,11 @@ def index(request):
         'textPredict' : Test,
         'sentimenPredict' : y_predsTest,
         'testingTable': fileTestResult,
+        'sentimenDataVisual': chart,
+        'countWord':top_words,
+        'wordDataVisual': chartWord,
+        'trainDataVisual' : chartTrain,
+        'reportDataVisual' : chartClassificationReport,
     }
     
     return render(request, 'sentimen/index.html', context)
