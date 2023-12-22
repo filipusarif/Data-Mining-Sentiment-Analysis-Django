b3b320aee8efee0030aa4fe4d358a87b8e549dab
filipusarif <filipus123.gmil@gmail.com>
2023-12-22 20:32:53 +0700

    editing ui

--- DM_Django/DM_Django/settings.py
+++ DM_Django/DM_Django/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-kcw^0&$kf@ef2s%11^##tos)h@uwtpgbgfi9b1#zo0@0058oe(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
